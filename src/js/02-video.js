0365928ec6f8e179fe0e3aed1f992a6fc091f64f
Tetiana Doroshko <yanisolua@gmail.com>
2022-05-27 18:07:17 +0300

    adds resolves of task-02.

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+const throttle = require('lodash.throttle');
 
+const video = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(video);
+
+const KEY = 'videoplayer-current-time';
+
+const storage = {
+  setItem(time) {
+    localStorage.setItem(KEY, JSON.stringify(time));
+  },
+  getItem(key) {
+    try {
+      return JSON.parse(localStorage.getItem(key));
+    } catch {
+      console.log('Error of parsing data from Local Storage');
+    }
+  },
+};
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+function saveCurrentTime(event) {
+  storage.setItem(event.seconds);
+}
+
+if (localStorage.getItem(KEY)) {
+  player.setCurrentTime(storage.getItem(KEY));
+}
